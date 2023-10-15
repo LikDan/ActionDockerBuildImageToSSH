a78d2de36c807f5beca460d00c02d8fec5912287
LikDan <likdan.company@gmail.com>
2023-10-15 20:29:26 +0300

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ docker build -f "$3"/Dockerfile -t $1 .
 docker save --output container.tar container:latest
 
 echo "$5" >> 'key.pem'
+chmod 400 'key.pem'
 
 scp -i 'key.pem' -o 'StrictHostKeyChecking no' container.tar "$4":container.tar
 ssh -i "key.pem" -o 'StrictHostKeyChecking no' -t "$4" "sudo docker load --input container.tar && rm container.tar"
