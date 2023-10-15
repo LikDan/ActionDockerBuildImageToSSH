d8dbd23685cfa835a767af090e6077d2bb10eeaf
LikDan <likdan.company@gmail.com>
2023-10-15 22:25:05 +0300

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@
 cd "$2" || exit
 
 docker build -f "$3"/Dockerfile -t "$1" .
-docker save --output container.tar container:latest
+docker save --output container.tar "$1"
 
 echo "$5" > 'key.pem'
 chmod 600 'key.pem'
 
 scp -i 'key.pem' -o 'StrictHostKeyChecking no' container.tar "$4":container.tar
-ssh -i "key.pem" -o 'StrictHostKeyChecking no' -t "$4" "sudo docker rmi container:latest && sudo docker load --input container.tar && rm container.tar"
+ssh -i "key.pem" -o 'StrictHostKeyChecking no' -t "$4" 'sudo docker rmi $1 && sudo docker load --input container.tar && rm container.tar'
 
 rm container.tar
