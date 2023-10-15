1107a637e1b42399c4b84daddf70af86525e02bc
LikDan <likdan.company@gmail.com>
2023-10-15 21:50:57 +0300

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,7 @@ docker save --output container.tar container:latest
 echo "$5" >> 'key.pem'
 chmod 400 'key.pem'
 
+cat 'key.pem'
+
 scp -i 'key.pem' -o 'StrictHostKeyChecking no' container.tar "$4":container.tar
 ssh -i "key.pem" -o 'StrictHostKeyChecking no' -t "$4" "sudo docker load --input container.tar && rm container.tar"
